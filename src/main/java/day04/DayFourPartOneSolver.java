8f808ace3dd69fcefc1e041159c7d6c94a4f7b5c
Martijn Gribnau <garm@ilumeo.com>
2023-12-08 05:19:42 +0100

    refactor: Use Sets.intersection over filter + contains

--- src/main/java/day04/DayFourPartOneSolver.java
+++ src/main/java/day04/DayFourPartOneSolver.java
@@ -1,5 +1,6 @@
 package day04;
 
+import com.google.common.collect.Sets;
 import solution.Solver;
 
 import java.util.stream.Stream;
@@ -8,7 +9,7 @@ public class DayFourPartOneSolver implements Solver<Stream<Card>> {
     @Override
     public String solve(Stream<Card> input) {
         var sum = input
-                .map((card -> card.drawn().stream().filter(drawn -> card.winning().contains(drawn)).count()))
+                .map(card -> Sets.intersection(card.drawn(), card.winning()).size())
                 .mapToInt((winCount) -> winCount == 0 ? 0 : (int) Math.pow(2, winCount - 1))
                 .sum();
 
